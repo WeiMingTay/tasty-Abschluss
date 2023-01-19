d1c5ca0d1887b41cd2ca064df9bccc97367aee08
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2023-01-20 02:03:26 +0700

    app.js vorbereitet

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,5 @@
+const NavBar = () => {
+	return <nav><h3>Nav</h3></nav>;
+};
+
+export default NavBar;
---
+++ src/Pages/AreasPage/AreasPage.js
@@ -0,0 +1,10 @@
+import NavBar from "../../Components/NavBar/NavBar";
+
+const AreasPage = () => {
+	return <section>
+        <h2>Areas</h2>
+        <NavBar />
+        </section>;
+};
+
+export default AreasPage;
