6a9c5fb83d5ebbb74c847e3014c727e55149bb93
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2023-01-25 23:44:10 +0800

    start/welcome ergänzt, box-shadow auf .card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 // import { data } from '../../Services/DummyData';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Card = ({ card }) => {
-    return (
-        <div className="container">
-            <Link className="card-link" to={`/category/${card.strCategory}`}>
-                <div className="card" key={card.idCategory}>
-                    <img src={card.strCategoryThumb} alt={card.strCategory} />
-                    <h2>{card.strCategory}</h2>
-                </div>
-            </Link>
-        </div>
-    );
+	return (
+		<div className="container">
+			<Link className="card-link" to={`/category/${card.strCategory}`}>
+				<div className="card" key={card.idCategory}>
+					<div>
+						<img src={card.strCategoryThumb} alt={card.strCategory} />
+					</div>
+					<h2>{card.strCategory}</h2>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Card;
-
