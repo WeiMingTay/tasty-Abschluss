3933a65e0ee46613ecfae85892e2fc07f7bb3cb4
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2023-01-26 19:35:01 +0800

    setTimeout der WelcomePage wieder auf 1500ms gesetzt

--- src/Pages/WelcomePage/WelcomePage.js
+++ src/Pages/WelcomePage/WelcomePage.js
@@ -15,7 +15,7 @@ const WelcomePage = () => {
 			if (location.pathname === "/") {
 				window.location.href = "/start";
 			}
-		}, 1500999);
+		}, 1500);
 	}, [location.pathname]);
 
 	/* Hier würde ich, wenn Zeit bleibt, noch eine "Ladeanimation" bauen... und dann die Timeoutzeit auf circa 2-3 Sek stellen */
