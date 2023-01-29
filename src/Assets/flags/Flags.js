c17be81f7e31e17e4878b031a64452bd88164cf2
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2023-01-30 00:23:36 +0800

    searchbar css

---
+++ src/Assets/flags/Flags.js
@@ -0,0 +1,61 @@
+	
+let flags = {
+{strArea: "American";
+code: "us"};
+{strArea: "British";
+code: "uk"};
+}
+/* 0	
+strArea	"American"
+1	
+strArea	"British"
+2	
+strArea	"Canadian"
+3	
+strArea	"Chinese"
+4	
+strArea	"Croatian"
+5	
+strArea	"Dutch"
+6	
+strArea	"Egyptian"
+7	
+strArea	"French"
+8	
+strArea	"Greek"
+9	
+strArea	"Indian"
+10	
+strArea	"Irish"
+11	
+strArea	"Italian"
+12	
+strArea	"Jamaican"
+13	
+strArea	"Japanese"
+14	
+strArea	"Kenyan"
+15	
+strArea	"Malaysian"
+16	
+strArea	"Mexican"
+17	
+strArea	"Moroccan"
+18	
+strArea	"Polish"
+19	
+strArea	"Portuguese"
+20	
+strArea	"Russian"
+21	
+strArea	"Spanish"
+22	
+strArea	"Thai"
+23	
+strArea	"Tunisian"
+24	
+strArea	"Turkish"
+25	
+strArea	"Unknown"
+26	
+strArea	"Vietnamese" */
